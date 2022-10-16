992ab689eb7f0e8416f655a0b66d91a6f3764fdd
gabriel-piedade95 <45984859+gabriel-piedade95@users.noreply.github.com>
2022-10-16 18:26:51 -0300

    EP 8C completo funcionando

--- matriz.c
+++ matriz.c
@@ -63,7 +63,6 @@ registro * criaRegistro(int lin, int col, int val){
     R->coluna = col;
     R->valor = val;
     R->prox = NULL;
-
     return R;
 
 }
@@ -83,18 +82,16 @@ PILHA * criaPilha(){
 }
 
 void empilha(registro * R, PILHA * P){
-
+    
     if(P->tam == 0){
-
-        P->q->valor = R->valor;
-        P->q->coluna = R->coluna;
-        P->q->linha = R->linha;
+        
+        P->q = R;
         P->tam++;
-        free(R);
+        //free(R);
         return;
 
     }
-
+    
     R->prox = P->q;
     P->q = R;
 
@@ -104,6 +101,10 @@ void empilha(registro * R, PILHA * P){
 }
 
 registro * desempilha(PILHA * P){
+
+    if(P->tam == 0){
+        return NULL;
+    }
 
     registro * aux;
 
@@ -115,14 +116,16 @@ registro * desempilha(PILHA * P){
 
 }
 
-void destroiPilha(PILHA * p){
+void destroiPilha(PILHA * P){
     
     registro * aux;
-    while(p->tam > 0){
-        aux = desempilha(p);
+    while(P->tam > 0){
+        aux = desempilha(P);
         free(aux);
     }
-    free(p);
+    free(P->p);
+    free(P->q);
+    free(P);
 }
 
 
@@ -151,12 +154,12 @@ registro * encontraValor(int **M, int N){
 
 }
 
-//NÃO TESTEI ESSA AINDA!!
-int ehVizinho(int lin, int col, int **M, registro * R){
+int ehVizinho(int lin, int col, registro * R, int N){
 
     
 
-    if (((abs(lin - R->linha) == 2) & (abs(col - R->coluna) == 1)) == 1 || ((abs(lin - R->linha) == 1) & (abs(col - R->coluna) == 2)) == 1){
+    if ((R->linha + lin < N) && (R->coluna + col <N) &&
+    (R->linha + lin >= 0) && (R->coluna + col >= 0)){
         return 1;
     }
 
@@ -164,51 +167,71 @@ int ehVizinho(int lin, int col, int **M, registro * R){
 
 }
 
-//AINDA NÃo ESTA PRONTA!!
+
 int cavalo(int **M, int n){
 
-    int i, j;
-    int x = -1;
+    int i, j, a, b;
     PILHA * P = criaPilha();
     registro * R = encontraValor(M, n);
     empilha(R, P);
-    int n_vizinhos;
+    int achou;
     registro * aux;
+
+    static const int plin[8] = { 2,  1, -1, -2, -2, -1,  1,  2};
+    static const int pcol[8] = { 1,  2,  2,  1, -1, -2, -2, -1}; 
+
+
     while(P->tam > 0){
 
         aux = desempilha(P);
+        achou = 0;
+        i = 0; j = 0;
 
-        n_vizinhos = 0;
+        while(i < 8 && j < 8){
 
-        if(aux->valor == n*n){
-            x = 1;
-            break;
-        }
+            if(ehVizinho(plin[i], pcol[j], aux, n) && achou == 0){
+               
+                a = aux->linha+plin[i];
+                b = aux->coluna+pcol[j];
 
-        
-        else{
-                for(i = 0; i < n; i++){
-                    for(j = 0; j < n; j++){
-                        if(ehVizinho(i, j, M, aux)){
-                            if(M[i][j] == 0){
-                                empilha(aux, P);
-                                n_vizinhos++;
-                            }
-                            
-                        }
-                    }
+            
+                if(M[a][b] == 0){
+                    
+                    empilha(criaRegistro(a, b, aux->valor+1), P);
+                    
+                    M[a][b] = aux->valor+1;
+                    
+                    achou = 1;
+
                 }
             }
-        
-        if(n_vizinhos == 0){
-            M[aux->linha][aux->coluna] = 0;
+            i++; j++;
+
         }
+        
+        if(aux->valor == n*n){
+            return 1;
+        }
+
+        if(achou == 0){
+            M[a][b] = 0;
+        }
+        
     }
 
-    free(aux);
-    destroiPilha(P);
-    return x;
+    return -1;
     
+}
+
+void imprimeMatriz(int **M, int N){
+
+    int i, j;
+
+    for (i = 0; i < N; ++i) {
+       for (j = 0; j < N; ++j)
+         printf("%3d ", M[i][j]);
+       printf("\n");
+    }
 }
 
 void liberaMatriz(int **M, int n){
@@ -220,15 +243,27 @@ void liberaMatriz(int **M, int n){
 
 int main(){
     
-    int **M;
-    int N;
+
     int C;
+    int N;
+    int **M;
+    char nome[100];
     
-    M = leMatriz("tabuleiro52.txt", &N);
-    //C = cavalo(M, N);
-    liberaMatriz(M, N);
-    printf("%d\n", C);
+    scanf("%s", nome);
 
+    M = leMatriz(nome, &N);
+    C = cavalo(M, N);
 
+    if(C > 0){
+        printf("Solucao:\n");
+        imprimeMatriz(M, N);
+    }
+    else{
+        M = leMatriz(nome, &N);
+        imprimeMatriz(M, N);
+        
+    }
+    
+    
     return 0;
 }
